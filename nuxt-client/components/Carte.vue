28b721d02ef5b883b3c7265cd547b2d951cf9323
CedricNoyel <cedric.noyel@gmail.com>
2020-11-02 17:15:31 +0100

    ajout ajout série

--- nuxt-client/components/Carte.vue
+++ nuxt-client/components/Carte.vue
@@ -29,3 +29,43 @@ export default {
   },
 }
 </script>
+
+<style>
+.container {
+  margin: 30px auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.sub-container-1 {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  text-align: center;
+}
+
+.sub-container-2 {
+  margin: 20px auto;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
